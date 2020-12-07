ea408a58b1a2c361082e14ed4fedad13dff33151
ke6ch <19749261+ke6ch@users.noreply.github.com>
2020-12-07 21:31:35 +0900

    Update single quotation

--- swagger.js
+++ swagger.js
@@ -8,13 +8,13 @@ const options = {
     info: {
       title: 'ke6ch',
       version: '1.0.0',
-      description: "Lorem ipsum"
+      description: 'Lorem ipsum'
     },
-//    host: "example.com",
-//    basePath: "/api/v2",
-    schemes: ["http","https"]
+//    host: 'example.com',
+//    basePath: '/api/v2',
+    schemes: ["http","https"],
   },
-  apis: ['./routes/*.js']
+  apis: ['./routes/*.js'],
 };
  
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options)));
